353e77fb0a8df773ed4332f625eb79ca086a3591
Hades\IXI <ixi150@gmail.com>
2018-12-09 13:32:54 +0100

    added vector2Int extensions

---
+++ Extensions/Vector2IntExtensions.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Xunity.Extensions
+{
+    public static class Vector2IntExtensions
+    {
+        public static Vector2Int Modified(this Vector2Int vector, int? x = null, int? y = null)
+        {
+            if (x.HasValue) vector.x = x.Value;
+            if (y.HasValue) vector.y = y.Value;
+            return vector;
+        }
+
+        public static Vector2Int Multiply(this Vector2Int a, Vector2Int b)
+        {
+            a.Scale(b);
+            return a;
+        }
+
+        public static Vector2 ToVector2(this Vector2Int v)
+        {
+            return new Vector2(v.x, v.y);
+        }
+    }
+}
